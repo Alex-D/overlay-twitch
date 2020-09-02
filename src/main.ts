7b1e9ca7e0a908280f68e1d978452fac762d7418
Alex-D <contact@alex-d.fr>
2020-09-03 01:27:15 +0200

    feat: add icon animations and support for raid, host, etc

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,20 @@ import {attributesModule} from 'snabbdom/src/package/modules/attributes'
 import {classModule} from 'snabbdom/src/package/modules/class'
 import {VNode} from 'snabbdom/src/package/vnode'
 
-const KEEP_ALERTS = false
+const KEEP_ALERTS = true
 
-type IconId = 'money-bag'
+type IconId = 'bits' | 'star' | 'heart' | 'euro' | 'parachute' | 'tv'
 
-type AlertType = 'donation' | 'follow' | 'subscription'
+const ALERT_TYPES = [
+	'bits',
+	'donation',
+	'follow',
+	'host',
+	'raid',
+	'subscription',
+] as const
+type AlertTypeTuple = typeof ALERT_TYPES
+type AlertType = AlertTypeTuple[number]
 
 type StreamLabsEvent = {
 	for?: 'twitch_account'
@@ -18,6 +27,9 @@ type StreamLabsEvent = {
 	type: AlertType
 	message: {
 		name: string
+		amount?: string
+		raiders?: string
+		viewers?: string
 	}[]
 }
 
@@ -34,52 +46,89 @@ type State = {
 const CONFETTI_COLORS = ['#f2055c', '#f2e205', '#f2b705', '#f28705']
 const EVENT_SHOW_DELAY = 5000
 
-const SENTENCES = {
-	['donation' as AlertType]: [
+const ALERT_ICONS: {[key in AlertType]: IconId} = {
+	'bits': 'bits',
+	'donation': 'euro',
+	'follow': 'heart',
+	'host': 'tv',
+	'raid': 'parachute',
+	'subscription': 'star',
+}
+
+const ALERT_SENTENCES = {
+	['bits' as AlertType]: [
 		(name: string) => h('span', [
-			h('strong', name),
+			h('strong.alert--name', name),
 			` a offert un bout de sa paye !`,
 		]),
 		(name: string) => h('span', [
 			`Merci `,
-			h('strong', name),
+			h('strong.alert--name', name),
 			`, tu es désormais bien plus beau.`,
 		]),
 		(name: string) => h('span', [
 			`Merci `,
-			h('strong', name),
+			h('strong.alert--name', name),
+			` pour ta contribution à mes prochains VSTs :)`,
+		]),
+	],
+	['donation' as AlertType]: [
+		(name: string) => h('span', [
+			h('strong.alert--name', name),
+			` a offert un bout de sa paye !`,
+		]),
+		(name: string) => h('span', [
+			`Merci `,
+			h('strong.alert--name', name),
+			`, tu es désormais bien plus beau.`,
+		]),
+		(name: string) => h('span', [
+			`Merci `,
+			h('strong.alert--name', name),
 			` pour ta contribution à mes prochains VSTs :)`,
 		]),
 	],
 	['follow' as AlertType]: [
 		(name: string) => h('span', [
-			h('strong', name),
+			h('strong.alert--name', name),
 			` suit désormais cette chaîne !`,
 		]),
 		(name: string) => h('span', [
 			`Bonsoir à `,
-			h('strong', name),
+			h('strong.alert--name', name),
 			` qui nous a rejoint !`,
 		]),
 		(name: string) => h('span', [
 			`Serait-ce là `,
-			h('strong', name),
+			h('strong.alert--name', name),
 			` qui souscrit l'abonnement ?`,
+		]),
+	],
+	['host' as AlertType]: [
+		(name: string) => h('span', [
+			h('strong.alert--name', name),
+			` host désormais ce live !`,
+		]),
+	],
+	['raid' as AlertType]: [
+		(name: string) => h('span', [
+			h('strong.alert--name', name),
+			` a lancé un raid !`,
 		]),
 	],
 	['subscription' as AlertType]: [
 		(name: string) => h('span', [
-			h('strong', name),
+			h('strong.alert--name', name),
 			` est généreux et se paye un sub !`,
 		]),
 		(name: string) => h('span', [
 			`Merci `,
-			h('strong', name),
+			h('strong.alert--name', name),
 			` pour le sub, tu sens désormais bien meilleur.`,
 		]),
 		(name: string) => h('span', [
 			`Et c'est `,
-			h('strong', name),
+			h('strong.alert--name', name),
 			` qui lâche un sub. Tu es moins riche, mais moi plus ! Merci :)`,
 		]),
 	],
@@ -109,7 +158,7 @@ function icon(iconId: IconId): VNode {
 }
 
 function getSentence(type: AlertType, name: string): VNode {
-	const sentencesForType = SENTENCES[type]
+	const sentencesForType = ALERT_SENTENCES[type]
 
 	const sentence = sentencesForType[Math.floor(Math.random() * sentencesForType.length)]
 
@@ -119,21 +168,41 @@ function getSentence(type: AlertType, name: string): VNode {
 function showConfetti(): void {
 	confetti({
 		particleCount: 200,
-		angle: 60,
-		spread: 55,
-		origin: { x: 0 },
+		angle: 40,
+		spread: 150,
+		origin: { x: -0.07 },
 		colors: CONFETTI_COLORS,
+		zIndex: -1,
+		ticks: 100,
 	});
 }
 
 function addAlert(event: StreamLabsEvent): void {
 	const type = event.type
-	const name = event.message[0].name
+	const message = event.message[0]
+	const name = message.name
+
+	let title = type as string
+	switch (type) {
+		case 'bits':
+			title = `${message.amount} bits`
+			break;
+		case 'host':
+			title = `Host — ${message.viewers} viewers`
+			break;
+		case 'raid':
+			title = `Raid — ${message.raiders} raiders`
+			break;
+	}
+
 	const messageVNode = h('div.alert', {
 		key: event.event_id,
 	}, [
-		icon('money-bag'),
-		getSentence(type, name),
+		icon(ALERT_ICONS[type]),
+		h('span.alert--title', title),
+		h('div.alert--message', [
+			getSentence(type, name),
+		]),
 	])
 
 	const alert: OverlayAlert = {
@@ -148,14 +217,8 @@ function addAlert(event: StreamLabsEvent): void {
 	showConfetti()
 }
 
-const SUPPORTED_EVENT_TYPES = [
-	'donation',
-	'follow',
-	'subscription',
-]
-
 streamlabs.on('event', (event: StreamLabsEvent) => {
-	if (!SUPPORTED_EVENT_TYPES.includes(event.type)) {
+	if (!ALERT_TYPES.includes(event.type)) {
 		return
 	}
 
