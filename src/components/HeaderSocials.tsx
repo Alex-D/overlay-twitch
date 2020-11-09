f9c965d583380bee6f9de412eae74f129c758411
Alex-D <contact@alex-d.fr>
2020-11-09 19:32:38 +0100

    feat: animate header socials

--- src/components/HeaderSocials.tsx
+++ src/components/HeaderSocials.tsx
@@ -1,29 +1,46 @@
 import {h, VNode} from 'preact'
+import {useRef, useState} from 'preact/hooks'
 
 import Icon from '~src/components/Icon'
+import useInterval from '~src/hooks/useInterval'
 
 export default function HeaderSocials(): VNode {
+	const [currentItemIndex, setCurrentItemIndex] = useState(0)
+	const [itemCount, setItemCount] = useState(1)
+	const socialsItemsDomElement = useRef<HTMLDivElement>(null)
+
+	useInterval(() => {
+		setItemCount(socialsItemsDomElement.current.childNodes.length)
+		setCurrentItemIndex((currentItemIndex < itemCount - 1) ? currentItemIndex + 1 : 0)
+	}, 10000, false, [currentItemIndex, itemCount, socialsItemsDomElement])
+
+	const styles = {
+		'--header-socials-item-count': itemCount,
+		'--header-socials-current-item-index': currentItemIndex,
+	}
+
 	return (
 		<div class="header--socials">
-			<div class="header--socials-item">
-				<div class="header--socials-icon">
-					<Icon iconId="twitter"/>
+			<div class="header--socials-items" style={styles} ref={socialsItemsDomElement}>
+				<div class="header--socials-item">
+					<div class="header--socials-icon">
+						<Icon iconId="twitter"/>
+					</div>
+					<div class="header--socials-name">
+						Twitter
+					</div>
+					@AlexandreDemode
 				</div>
-				<div class="header--socials-name">
-					Twitter
+				<div class="header--socials-item">
+					<div class="header--socials-icon">
+						<Icon iconId="github"/>
+					</div>
+					<div class="header--socials-name">
+						GitHub
+					</div>
+					@Alex-D
 				</div>
-				@AlexandreDemode
-			</div>
-			<div class="header--socials-item">
-				<div class="header--socials-icon">
-					<Icon iconId="github"/>
-				</div>
-				<div class="header--socials-name">
-					GitHub
-				</div>
-				@Alex-D
-			</div>
-			<div class="header--socials-item">
+				<div class="header--socials-item">
 				<div class="header--socials-icon">
 					<Icon iconId="soundcloud"/>
 				</div>
@@ -31,6 +48,7 @@ export default function HeaderSocials(): VNode {
 					SoundCloud
 				</div>
 				@AlexandreDemode
+			</div>
 			</div>
 		</div>
 	)
