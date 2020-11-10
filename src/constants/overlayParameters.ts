fe6e0db15747fe9e6573427ae94937c4209e724a
Alex-D <contact@alex-d.fr>
2020-11-10 17:46:53 +0100

    fix: make query parameter work in prod

--- src/constants/overlayParameters.ts
+++ src/constants/overlayParameters.ts
@@ -1,6 +1,18 @@
 import getUrlSearchParam from 'src/functions/getUrlSearchParam'
 
-export const STREAMLABS_SOCKET_API_TOKEN = import.meta.env.MODE === 'production' ? getUrlSearchParam('streamlabsToken') : import.meta.env.VITE_STREAMLABS_SOCKET_API_TOKEN
-export const TWITCH_CLIENT = import.meta.env.MODE === 'production' ? getUrlSearchParam('twitchClient') : import.meta.env.VITE_TWITCH_CLIENT
-export const TWITCH_SECRET = import.meta.env.MODE === 'production' ? getUrlSearchParam('twitchSecret') : import.meta.env.VITE_TWITCH_SECRET
-export const TWITCH_CHANNEL_ID = import.meta.env.MODE === 'production' ? getUrlSearchParam('twitchChannelId') : import.meta.env.VITE_TWITCH_CHANNEL_ID
+const getParameter = (key: string) => {
+	const sessionStorageValue = sessionStorage.getItem(key)
+	if (sessionStorageValue !== null) {
+		return sessionStorageValue
+	}
+
+	const value = getUrlSearchParam(key)
+	sessionStorage.setItem(key, value)
+
+	return value
+}
+
+export const STREAMLABS_SOCKET_API_TOKEN = import.meta.env.MODE === 'production' ? getParameter('streamlabsToken') : import.meta.env.VITE_STREAMLABS_SOCKET_API_TOKEN
+export const TWITCH_CLIENT = import.meta.env.MODE === 'production' ? getParameter('twitchClient') : import.meta.env.VITE_TWITCH_CLIENT
+export const TWITCH_SECRET = import.meta.env.MODE === 'production' ? getParameter('twitchSecret') : import.meta.env.VITE_TWITCH_SECRET
+export const TWITCH_CHANNEL_ID = import.meta.env.MODE === 'production' ? getParameter('twitchChannelId') : import.meta.env.VITE_TWITCH_CHANNEL_ID
